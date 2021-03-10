564e0de422a18bae30baf2ba736728cb992cde15
Mudra Bhandari <mudrabhandari123@yahoo.in>
2021-03-10 15:09:34 +0530

    Registration Page UI complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cab_rider/screens/loginpage.dart';
 import 'package:cab_rider/screens/mainpage.dart';
+import 'package:cab_rider/screens/registration_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Brand-Regular',
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: RegistrationPage(),
     );
   }
 }
---
+++ lib/screens/registration_page.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+import '../brand_colors.dart';
+
+class RegistrationPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 70,
+                ),
+                Image(
+                  alignment: Alignment.center,
+                  height: 100.0,
+                  width: 100.0,
+                  image: AssetImage("images/logo1.png"),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  "Create a Rider's Account!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25.0, fontFamily: 'Brand-Bold'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      //Full name
+                      TextField(
+                        keyboardType: TextInputType.name,
+                        decoration: InputDecoration(
+                            labelText: 'Full name',
+                            labelStyle: TextStyle(
+                              fontSize: 14.0,
+                            ),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            )),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      //Email Address
+                      TextField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            labelText: 'Email address',
+                            labelStyle: TextStyle(
+                              fontSize: 14.0,
+                            ),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            )),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      // Phone Number
+                      TextField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                            labelText: 'Phone number',
+                            labelStyle: TextStyle(
+                              fontSize: 14.0,
+                            ),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            )),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                              fontSize: 14.0,
+                            ),
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 10.0,
+                            )),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      SizedBox(
+                        height: 14,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        /*color: BrandColors.colorBlueGray,
+                        textColor: Colors.white,
+                        shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(25.0)),*/
+                        style: ButtonStyle(shape: MaterialStateProperty.all(
+                            new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(25.0)
+                            )
+                        ),
+                            foregroundColor: MaterialStateProperty.all(Colors.white),
+                            backgroundColor: MaterialStateProperty.all(BrandColors.colorBlueGray)),
+                        child: Container(
+                          height: 50,
+                          child: Center(
+                            child: Text(
+                              'REGISTER',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontFamily: 'Brand-Bold',
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                TextButton(
+                  onPressed: (){},
+                  child: Text("Already have a Rider's Account? Login!"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
